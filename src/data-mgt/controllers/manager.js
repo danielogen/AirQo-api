fa39449deb0fd7f5c184b66d20c5f4a3a887f7ba
Martin <bbaalmart@gmail.com>
2020-02-24 09:56:33 +0300

    extended the limit from 100 to 8000 when getting air quality values

--- src/data-mgt/controllers/manager.js
+++ src/data-mgt/controllers/manager.js
@@ -39,7 +39,7 @@ const data = {
         res.status(200).send(json);
     },
     storeFeeds: async (req, res) => {
-        const api_url = `https://api.thingspeak.com/channels/${req.params.ch_id}/feeds.json`;
+        const api_url = `https://api.thingspeak.com/channels/${req.params.ch_id}/feeds.json?results=8000`;
         const fetch_response = await fetch(api_url);
         const json = await fetch_response.json();
         res.status(200).send(json);
